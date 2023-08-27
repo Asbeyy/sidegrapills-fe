27a02a6d6962d392ef34f730a3935dccbed7ff8d
Asbeyy <114877785+Asbeyy@users.noreply.github.com>
2023-08-28 01:21:58 +0200

    fc

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,4 @@ import ReactDOM from "react-dom/client";
 import Home from "./components/home/Home.jsx";
 import Scene from "./components/threejs/Scene.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<Home />);
+ReactDOM.createRoot(document.getElementById("root")).render(<Scene />);
